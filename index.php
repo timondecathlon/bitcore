8f0591b59968d6bb9dae12402aec21bb317a2833
timondecathlon <sonicstyle99@gmail.com>
2020-12-08 17:03:39 +0300

    some stuff from local

--- index.php
+++ index.php
@@ -4,8 +4,8 @@ include_once(__DIR__ . '/config.php');
 
 \Bitkit\Core\Entities\Unit::test();
 
-\Bitkit\Core\Entities\Unit::test(); 
-
 \Bitkit\Core\Entities\Unit::testSomeElse();  
+
+echo 1111;
 
 
