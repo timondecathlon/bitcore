c94fe10c75342bc9c31a8c5ba569561b63566cbd
timondecathlon <sonicstyle99@gmail.com>
2020-12-10 13:28:56 +0300

    test1

--- index.php
+++ index.php
@@ -6,13 +6,7 @@ include_once(__DIR__ . '/config.php');
 
 \Bitkit\Core\Entities\Unit::testSomeElse();  
 
-echo 1111;
-
-echo 3333; 
-
-
-echo 55555;  
-
-echo 'echo';  
+echo 'final deploy test';
+ 
 
 
