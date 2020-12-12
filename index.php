526809b6eee9c53e63665157582ad7b546139526
timonusa <bitkitpro@gmail.com>
2020-12-12 23:37:45 +0300

    342

--- index.php
+++ index.php
@@ -3,11 +3,11 @@
 
 include_once(__DIR__ . '/config.php');      
 
-var_dump(\Bitkit\Core\Entities\Card::getAllLines());       
+//var_dump(\Bitkit\Core\Entities\Card::getAllLines());       
 
 $card = new \Bitkit\Core\Entities\Card(2);
 
-var_dump($card->getData()); 
+var_dump($card->getLine());  
  
 
 
