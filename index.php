8c3078d880948e3e46b68732e6f3471bba474620
timondecathlon <timondecathlon@gmail.com>
2020-12-12 17:44:05 +0300

    111233

--- index.php
+++ index.php
@@ -6,7 +6,9 @@ include_once(__DIR__ . '/config.php');
 
 \Bitkit\Core\Entities\Unit::testSomeElse();  
 
-echo 'final deploy test 1111111222333';  
+echo 'final deploy test 1111111222333';
+
+echo 1111;
  
 
 
