8cd7f433a9ef96b04d026e765a2cc27179435023
timondecathlon <sonicstyle99@gmail.com>
2020-12-18 14:52:13 +0300

    test2

--- system/classes/Bitkit/Core/Patterns/Factory.php
+++ system/classes/Bitkit/Core/Patterns/Factory.php
@@ -8,12 +8,13 @@ namespace Bitkit\Core\Patterns;
  */
 abstract class Factory
 {
-    public static function createClass($tableName)  
+    public static function createClass(string $tableName)  
     {
         return new class($tableName) extends \Bitkit\Core\Entities\Unit {
+
             const TABLE_NAME = $tableName;
             
-        }
+        };  
     }
 
 
