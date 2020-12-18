887620ff41b75ad2d56e821fe67c1f3abe009c28
timondecathlon <sonicstyle99@gmail.com>
2020-12-18 14:50:13 +0300

    test2

--- index.php
+++ index.php
@@ -9,4 +9,19 @@ ini_set('display_startup_errors', 1);
            
 
 
+include_once(__DIR__ . '/config.php');      
+
+//var_dump(\Bitkit\Core\Entities\Card::getAllLines());       
+
+$card = new \Bitkit\Core\Entities\Card(2);
+
+//var_dump($card->getLine());  
+
+$factory = new \Bitkit\Core\Patterns\Factory();
+
+$posts = $factory::createClass('core_cards'); 
+
+
+var_dump($posts::getAllLines()); 
+
 
